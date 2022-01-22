52b36667010f181bc9f3eddff749a6461a3e8aaf
Dorothy <dthuc.nguyen.120@gmail.com>
2022-01-22 00:20:25 -0500

    server setup

---
+++ server/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send({data:"hello"});
+});
+
+exports.app = functions.https.onRequest(app);
